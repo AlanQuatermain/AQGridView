a012e99e42587fbd70b60fd880b05e7800391266
Jim Dovey <jdovey@kobobooks.com>
2010-08-16 12:11:12 -0400

    Implemented a new demo project to show iPhoto-style expansion of grid items from a single point.

---
+++ Examples/ExpanderDemo/Classes/ImageGridViewCell.m
@@ -0,0 +1,103 @@
+/*
+ * ImageGridViewCell.m
+ * Classes
+ * 
+ * Created by Jim Dovey on 16/8/2010.
+ * 
+ * Copyright (c) 2010 Jim Dovey
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the project's author nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#import "ImageGridViewCell.h"
+
+@implementation ImageGridViewCell
+
+- (id) initWithFrame: (CGRect) frame reuseIdentifier: (NSString *) aReuseIdentifier
+{
+	self = [super initWithFrame: frame reuseIdentifier: aReuseIdentifier];
+	if ( self == nil )
+		return ( nil );
+	
+	_imageView = [[UIImageView alloc] initWithFrame: CGRectZero];
+	[self.contentView addSubview: _imageView];
+	
+	return ( self );
+}
+
+- (void) dealloc
+{
+	[_imageView release];
+	[super dealloc];
+}
+
+- (CALayer *) glowSelectionLayer
+{
+    return ( _imageView.layer );
+}
+
+- (UIImage *) image
+{
+    return ( _imageView.image );
+}
+
+- (void) setImage: (UIImage *) anImage
+{
+    _imageView.image = anImage;
+    [self setNeedsLayout];
+}
+
+- (void) layoutSubviews
+{
+    [super layoutSubviews];
+    
+    CGSize imageSize = _imageView.image.size;
+    CGRect frame = _imageView.frame;
+    CGRect bounds = self.contentView.bounds;
+    
+    if ( (imageSize.width <= bounds.size.width) &&
+		(imageSize.height <= bounds.size.height) )
+    {
+        return;
+    }
+    
+    // scale it down to fit
+    CGFloat hRatio = bounds.size.width / imageSize.width;
+    CGFloat vRatio = bounds.size.height / imageSize.height;
+    CGFloat ratio = MAX(hRatio, vRatio);
+    
+    frame.size.width = floorf(imageSize.width * ratio);
+    frame.size.height = floorf(imageSize.height * ratio);
+    frame.origin.x = floorf((bounds.size.width - frame.size.width) * 0.5);
+    frame.origin.y = floorf((bounds.size.height - frame.size.height) * 0.5);
+    _imageView.frame = frame;
+}
+
+@end
